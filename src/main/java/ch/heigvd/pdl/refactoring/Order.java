618e3684c2ecd534392c35539d023c6077a338dd
Kumo-chan <71428805+Kumo-chan@users.noreply.github.com>
2023-06-09 15:31:59 +0200

    Mode order toString logique to orders

--- src/main/java/ch/heigvd/pdl/refactoring/Order.java
+++ src/main/java/ch/heigvd/pdl/refactoring/Order.java
@@ -26,4 +26,18 @@ public class Order {
     public void addProduct(Product product) {
         products.add(product);
     }
+    @Override
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder("{\"id\": ");
+        stringBuilder.append(id);
+        stringBuilder.append(", \"products\": [");
+        for (Product product : products) {
+            stringBuilder.append(product);
+            stringBuilder.append(", ");
+        }
+        if (products.size() > 0) {
+            stringBuilder.delete(stringBuilder.length() - 2, stringBuilder.length());
+        }
+        return stringBuilder.toString();
+    }
 }
--- src/main/java/ch/heigvd/pdl/refactoring/OrdersWriter.java
+++ src/main/java/ch/heigvd/pdl/refactoring/OrdersWriter.java
@@ -13,20 +13,7 @@ public class OrdersWriter {
 
         for (int i = 0; i < orders.getOrdersCount(); i++) {
             Order order = orders.getOrder(i);
-            sb.append("{");
-            sb.append("\"id\": ");
-            sb.append(order.getOrderId());
-            sb.append(", ");
-            sb.append("\"products\": [");
-            for (int j = 0; j < order.getProductsCount(); j++) {
-                Product product = order.getProduct(j);
-                sb.append(product);
-                sb.append(", ");
-            }
-            if (order.getProductsCount() > 0) {
-                sb.delete(sb.length() - 2, sb.length());
-            }
-
+            sb.append(order);
             sb.append("]");
             sb.append("}, ");
         }
