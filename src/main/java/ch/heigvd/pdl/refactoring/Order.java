aa256614c28a67ff7257e015c2d566f91fc98238
Kumo-chan <71428805+Kumo-chan@users.noreply.github.com>
2023-06-09 15:33:03 +0200

    Remove useless method

--- src/main/java/ch/heigvd/pdl/refactoring/Order.java
+++ src/main/java/ch/heigvd/pdl/refactoring/Order.java
@@ -11,18 +11,6 @@ public class Order {
         this.id = id;
     }
 
-    public int getOrderId() {
-        return id;
-    }
-
-    public int getProductsCount() {
-        return products.size();
-    }
-
-    public Product getProduct(int j) {
-        return products.get(j);
-    }
-
     public void addProduct(Product product) {
         products.add(product);
     }
